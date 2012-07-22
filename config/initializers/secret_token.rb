215adedfcd1eb529a05b56318edd98281d08b8b9
Alfonso Ayala <alfonsoayalapaloma@gmail.com>
2012-07-22 17:57:13 -0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kerberos::Application.config.secret_token = '865079589f9034827b7bcefa6531e45d2f7b91cb3a8f641a1e707153111d9356f823c836e1136afba5f272386a968b278fd0dc1055c86cd190551204d090d86e'
